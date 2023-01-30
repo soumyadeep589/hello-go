73e0065cb1d2cdfc2223833401e9696dfa55b4ed
Soumyadeep Mondal <soumyadeepmondal@Soumyadeeps-MacBook-Air.local>
2023-01-30 23:21:14 +0530

    initial

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"example/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
+
+
+
+func main() {
+    fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
